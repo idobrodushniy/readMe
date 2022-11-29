f1f77983ca9f1d5bb52f4bf8a8ab160a314fa30d
Illia Petrov <idobrodushniy@gmail.com>
2022-11-28 18:32:53 -0800

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Ilia on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
